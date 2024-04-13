69ee858bb80a59dd52949520142a4d3948d0d569
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-04-13 15:53:44 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -47,3 +48,6 @@ Route::get('/produk/{id}', function($id){
 Route::get('/produk-redirect/{id}', function($id){
     return redirect()->route('product.detail', ['id' => $id]);
 });
+
+Route::get('/controller/hello/request', [HelloController::class, 'request']);
+Route::get('/controller/hello/{name}', [HelloController::class, 'hello']);
