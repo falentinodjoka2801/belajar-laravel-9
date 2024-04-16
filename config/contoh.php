c904feaf7b983024f41f2af5a1d88b20e6261d5a
tinooasis <falentinodjoka2801@gmail.com>
2024-04-16 20:47:33 +0700

    Configuration (buat file contoh config tambahan, dan testing)

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        'name'  =>  [
+            'first' =>  env('NAME_FIRST', 'Tino'),
+            'last'  =>  env('NAME_LAST', 'Oasis')
+        ],
+        'email' =>  'falentinodjoka2801@gmail.com',
+        'web'   =>  'tinooasis.com'
+    ];
+?>
