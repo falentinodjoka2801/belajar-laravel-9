dcd80e5c05774c6f1cbacc023cdbc0bda999e55f
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-04-24 15:20:23 +0700

    Exception: throw exception. catch (report) exception di handler.php, dsb

---
+++ app/Exceptions/ValidationError.php
@@ -0,0 +1,11 @@
+<?php
+    namespace App\Exceptions;
+
+    use Exception;
+
+    class ValidationError extends Exception{
+        public function __construct(string $message){
+            parent::__construct($message);
+        }
+    }
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Exceptions\ValidationError;
 use App\Http\Controllers\CookieController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\HelloController;
@@ -91,3 +92,16 @@ Route::get('/middleware/group', function(){
 
 Route::get('/session/create', [SessionController::class, 'createSession']);
 Route::get('/session/get', [SessionController::class, 'getSession']);
+
+Route::get('/error/sample', function(){
+    throw new Exception('Sample Error');
+});
+
+Route::get('/error/manual', function(){
+    report(new Exception('Sample Error'));
+    return 'ok';
+});
+
+Route::get('/error/validation', function(){
+    throw new ValidationError('Validation Error');
+});
