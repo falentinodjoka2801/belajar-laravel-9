81db9e6245d0337c624fa000faf89e33a51cfc46
Tino Oasis Laptop <falentinodjoka2801@gmail.com>
2024-04-24 15:39:56 +0700

    HTTP Exception: make and throw exception based on http status code

--- routes/web.php
+++ routes/web.php
@@ -105,3 +105,15 @@ Route::get('/error/manual', function(){
 Route::get('/error/validation', function(){
     throw new ValidationError('Validation Error');
 });
+
+Route::get('/abort/400', function(){
+    abort(400, 'Ups, ada error');
+});
+
+Route::get('/abort/401', function(){
+    abort(401);
+});
+
+Route::get('/abort/500', function(){
+    abort(500);
+});
